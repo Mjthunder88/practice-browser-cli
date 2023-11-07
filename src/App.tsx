7ee943fd9d8ec5639723974c2ca1f18ed164af8f
Marcus Johnson <marcusjohnson88.mj@gmail.com>
2023-11-06 18:00:47 -0700

    got it to render react components in the browser and refresh before a user submits their code

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,9 @@ import * as esbuild from "esbuild-wasm";
 import { unpkgPathPlugin } from "./plugins/UnpkgPlugin";
 import { fetchPlugin } from "./plugins/fetch-plugin";
 
-
-
 function App() {
-  const ref = useRef<any>()
+  const ref = useRef<any>();
+  const iframe = useRef<any>();
   const [input, setInput] = useState("");
   const [code, setCode] = useState("");
 
@@ -23,32 +22,49 @@ function App() {
 
   const submitCodeHandler = async () => {
     if (!ref.current) {
-      return
+      return;
     }
 
+    // * Resets the iframe before it transpiles and bundles the code the user put in.
+    iframe.current.srcdoc = html;
+
     const result = await ref.current.build({
-      entryPoints: ['index.js'],
+      entryPoints: ["index.js"],
       bundle: true,
       write: false,
       plugins: [unpkgPathPlugin(), fetchPlugin(input)],
       define: {
-        'process.env.NODE_ENV': '"production"',
-        global: 'window'
-      }
-    })
+        "process.env.NODE_ENV": '"production"',
+        global: "window",
+      },
+    });
 
     // * Defining will find these instances of variables that haven't been defined in a module and will replace its value with whatever you set it to be.
 
-    setCode(result.outputFiles[0].text);
+    // setCode(result.outputFiles[0].text);
 
-
+    iframe.current.contentWindow.postMessage(result.outputFiles[0].text, "*");
   };
   // * the target till target what esbuild will try and transpile the users code too
 
-const html = `
-<script>
-${code}
-</script>
+  const html = `
+  <html>
+    <head></head>
+    <body>
+    <div id="root"></div>  
+      <script>
+        window.addEventListener('message', (event) => {
+          try {
+            eval(event.data);
+          } catch (err) {
+            const root = document.querySelector('#root');
+            root.innerHTML = '<div style="color: red;"><h4>Runtim Error</h4>' + err + '</div>'
+            console.error(err);
+          }
+        }, false);
+      </script>
+    </body>
+  </html>
 `;
 
   return (
@@ -61,7 +77,12 @@ ${code}
         <button onClick={submitCodeHandler}>Submit</button>
       </div>
       <pre>{code}</pre>
-      <iframe sandbox="allow-scripts" srcDoc={html}/>
+      <iframe
+        title="preview"
+        ref={iframe}
+        sandbox="allow-scripts"
+        srcDoc={html}
+      />
     </div>
   );
 }
